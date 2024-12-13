ecfdc1aa14ac5fafc402934713f387bfba607aa8
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-13 22:12:25 +0100

    refactor(basic-3.3): rename classes and identifiers for concise style

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
     
-    listItem.className='list-item';
+    listItem.className='item';
 
     label.innerText=taskString;
     label.className='label task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.className="checkbox-input";
+    checkBox.className="checkbox";
     editInput.type="text";
     editInput.className="task text-input";
 
@@ -89,7 +89,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -103,7 +103,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit");
 };
 
 
@@ -159,7 +159,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector(".checkbox-input");
+    var checkBox=taskListItem.querySelector(".checkbox");
     var editButton=taskListItem.querySelector("button.edit");
     var deleteButton=taskListItem.querySelector("button.delete");
 
